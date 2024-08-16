6d6a55c7001579b0f8db7f4aa9824f4f758ad9b5
Srinivas <srinivas.teella@vivaenergy.com.au>
2024-08-16 13:22:49 +1000

    initial commit

---
+++ src/controllers/users.ts
@@ -0,0 +1,55 @@
+import express from "express";
+import { deleteUserById, getUserById, getUsers } from "../db/user";
+
+export const getAllUsers = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const users = await getUsers();
+    return res.status(200).json(users).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.sendStatus(400);
+    }
+    const deletedUser = await deleteUserById(id);
+    return res.json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { userName } = req.body;
+    if (!userName) {
+      return res.sendStatus(400);
+    }
+    const user = await getUserById(id);
+    if (!user) return res.send("User not found");
+
+    user.userName = userName;
+
+    user.save();
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
